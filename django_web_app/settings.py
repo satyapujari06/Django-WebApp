2be077b538f9c87ed080f46a228e72224f136e13
satyapujari06 <158019889+satyapujari06@users.noreply.github.com>
2024-06-19 16:47:49 +0530

    Update settings.py

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'django_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'booking',
+        'POST':'3306',
+        'PASSWORD':'admin123456',
+        'HOST':'webapp.c528kg6649yp.us-east-1.rds.amazonaws.com',
+        'USER':'admin'
     }
 }
 
