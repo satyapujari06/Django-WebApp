8c2f74c50ee13205dcf3ccaa361e5c3fb6e49a7c
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-07-03 12:06:22 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -2,12 +2,12 @@
 
 cd /var/lib/jenkins/workspace/django
 
-#sudo cp -rf nginx.conf /etc/nginx/sites-available/
+sudo cp -rf nginx.conf /etc/nginx/sites-available/
 sudo rm /etc/nginx/sites-enabled/nginx.conf
 sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
 chmod 710 /var/lib/jenkins/workspace/django 
 
-#sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
+sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
 sudo service nginx reload
 sudo service nginx start
 sudo service nginx enable
