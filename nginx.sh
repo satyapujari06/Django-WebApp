4fcb2dafdd5e296e8f7eed5657b60a801009fed3
Praveenchinna14 <162446658+Praveenchinna14@users.noreply.github.com>
2024-07-03 10:09:36 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -8,10 +8,10 @@ sudo ln -s /etc/nginx/sites-available/ /etc/nginx/sites-enabled/nginx.conf
 chmod 710 /var/lib/jenkins/workspace/django 
 
 #sudo sed -i 's/80 default_server/81 default_server/g' /etc/nginx/sites-enabled/default
-sudo systemctl reload nginx
-sudo systemctl start nginx
-sudo systemctl enable nginx
+sudo service nginx reload
+sudo service nginx start
+sudo service nginx enable
 
 echo "Nginx has been started"
 
-sudo systemctl status nginx
+sudo service nginx status
